63f3a8b65696f3173f25723b3b31c5bddce59370
abhishek <abhishek@Abhi>
2019-10-03 01:25:51 +0530

    First Commit

---
+++ JenkinsTesting/src/main/java/JenkinsCheck/Callme.java
@@ -0,0 +1,17 @@
+package JenkinsCheck;
+
+import org.testng.annotations.Test;
+
+public class Callme {
+	
+	@Test
+	public void Hello(){
+		System.out.println("Hello People");
+	}
+	
+	@Test
+	public void Bye(){
+		System.out.println("Bye People");
+	}
+
+}
---
+++ JenkinsTesting/src/test/java/MainPackage/MainClass1.java
@@ -0,0 +1,17 @@
+package MainPackage;
+
+import JenkinsCheck.Callme;
+
+public class MainClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Callme cobj = new Callme();
+		cobj.Hello();
+		cobj.Bye();
+		
+		
+	}
+
+}
